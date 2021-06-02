56ef4b020525879c37a98f5e37985f4dea1f2435
kreangks <kreangks@bot.or.th>
2021-06-02 19:01:40 +0700

    improve layout body

--- lib/screens/quiz/components/body.dart
+++ lib/screens/quiz/components/body.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_web_02/constants.dart';
-import 'package:flutter_web_02/models/questions.dart';
 import 'package:flutter_web_02/screens/quiz/components/progress_bar.dart';
+import 'package:flutter_web_02/screens/quiz/components/question_card.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -18,16 +18,20 @@ class Body extends StatelessWidget {
           height: size.height,
         ),
         SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ProgressBar(),
-                SizedBox(
-                  height: kDefaultPadding,
-                ),
-                Text.rich(TextSpan(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  child: ProgressBar()),
+              SizedBox(
+                height: kDefaultPadding,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Text.rich(TextSpan(
                     text: 'Question 1',
                     style: Theme.of(context)
                         .textTheme
@@ -41,76 +45,17 @@ class Body extends StatelessWidget {
                               .headline5!
                               .copyWith(color: kSecondaryColor))
                     ])),
-                Divider(
-                  thickness: 1.5,
-                ),
-                SizedBox(
-                  height: kDefaultPadding,
-                ),
-                Container(
-                  padding: EdgeInsets.all(kDefaultPadding),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(25)),
-                  child: Column(
-                    children: [
-                      Text(sample_data[0]['question'],
-                          // style: TextStyle(color: kBlackColor),
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline4!
-                              .copyWith(color: kBlackColor)),
-                      Container(
-                        margin: EdgeInsets.only(top: kDefaultPadding),
-                        padding: EdgeInsets.all(kDefaultPadding),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: kGrayColor),
-                            borderRadius: BorderRadius.circular(15)),
-                        child: Row(
-                          children: [
-                            Text(
-                              "1. Test",
-                              style: TextStyle(color: kGrayColor, fontSize: 16),
-                            ),
-                            Spacer(),
-                            Container(
-                              height: 26,
-                              width: 26,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: kGrayColor),
-                                  borderRadius: BorderRadius.circular(50)),
-                            )
-                          ],
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: kDefaultPadding),
-                        padding: EdgeInsets.all(kDefaultPadding),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: kGrayColor),
-                            borderRadius: BorderRadius.circular(15)),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "2. margin: EdgeInsets.only(top: kDefaultPadding)",
-                              style: TextStyle(color: kGrayColor, fontSize: 16),
-                            ),
-                            Container(
-                              height: 26,
-                              width: 26,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: kGrayColor),
-                                  borderRadius: BorderRadius.circular(50)),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+              Divider(
+                thickness: 1.5,
+              ),
+              SizedBox(
+                height: kDefaultPadding,
+              ),
+              Expanded(
+                  child: PageView.builder(
+                      itemBuilder: (context, index) => QuestionCard())),
+            ],
           ),
         )
       ],
