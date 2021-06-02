93c8f80b907b7f0e760324ddcaa802c771e1175c
kreangks <kreangks@bot.or.th>
2021-06-02 16:55:00 +0700

    add question body

--- lib/screens/quiz/components/body.dart
+++ lib/screens/quiz/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_web_02/constants.dart';
+import 'package:flutter_web_02/models/questions.dart';
 import 'package:flutter_web_02/screens/quiz/components/progress_bar.dart';
 
 class Body extends StatelessWidget {
@@ -20,7 +21,95 @@ class Body extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
             child: Column(
-              children: [ProgressBar()],
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ProgressBar(),
+                SizedBox(
+                  height: kDefaultPadding,
+                ),
+                Text.rich(TextSpan(
+                    text: 'Question 1',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4!
+                        .copyWith(color: kSecondaryColor),
+                    children: [
+                      TextSpan(
+                          text: '/10',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5!
+                              .copyWith(color: kSecondaryColor))
+                    ])),
+                Divider(
+                  thickness: 1.5,
+                ),
+                SizedBox(
+                  height: kDefaultPadding,
+                ),
+                Container(
+                  padding: EdgeInsets.all(kDefaultPadding),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Column(
+                    children: [
+                      Text(sample_data[0]['question'],
+                          // style: TextStyle(color: kBlackColor),
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline4!
+                              .copyWith(color: kBlackColor)),
+                      Container(
+                        margin: EdgeInsets.only(top: kDefaultPadding),
+                        padding: EdgeInsets.all(kDefaultPadding),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: kGrayColor),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Row(
+                          children: [
+                            Text(
+                              "1. Test",
+                              style: TextStyle(color: kGrayColor, fontSize: 16),
+                            ),
+                            Spacer(),
+                            Container(
+                              height: 26,
+                              width: 26,
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: kGrayColor),
+                                  borderRadius: BorderRadius.circular(50)),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: kDefaultPadding),
+                        padding: EdgeInsets.all(kDefaultPadding),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: kGrayColor),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "2. margin: EdgeInsets.only(top: kDefaultPadding)",
+                              style: TextStyle(color: kGrayColor, fontSize: 16),
+                            ),
+                            Container(
+                              height: 26,
+                              width: 26,
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: kGrayColor),
+                                  borderRadius: BorderRadius.circular(50)),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         )
--- lib/screens/quiz/components/progress_bar.dart
+++ lib/screens/quiz/components/progress_bar.dart
@@ -2,41 +2,49 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_web_02/constants.dart';
+import 'package:flutter_web_02/controllers/question_controller.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 
 class ProgressBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: 35,
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.white, width: 3),
-          borderRadius: BorderRadius.circular(50)),
-      child: Stack(
-        children: [
-          // Layout Builder ทำให้ควบคุม space ของ
-          // constraints.maxWidth เพื่อใช้ทำ animation
-          LayoutBuilder(
-              builder: (context, constraints) => Container(
-                    width: constraints.maxWidth * 0.25,
-                    decoration: BoxDecoration(
-                        gradient: kPrimaryGradient,
-                        borderRadius: BorderRadius.circular(50)),
-                  )),
-          Positioned.fill(
-              child: Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: kDefaultPadding / 2),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("18 sec"),
-                SvgPicture.asset('assets/icons/clock.svg')
-              ],
-            ),
-          ))
-        ],
-      ),
-    );
+        width: double.infinity,
+        height: 35,
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.white, width: 3),
+            borderRadius: BorderRadius.circular(50)),
+        child: GetBuilder<QuestionController>(
+            init: QuestionController(),
+            builder: (controller) {
+              // print(controller.animation.value);
+              return Stack(
+                children: [
+                  // Layout Builder ทำให้ควบคุม space ของ
+                  // constraints.maxWidth เพื่อใช้ทำ animation
+                  LayoutBuilder(
+                      builder: (context, constraints) => Container(
+                            width: constraints.maxWidth *
+                                controller.animation.value,
+                            decoration: BoxDecoration(
+                                gradient: kPrimaryGradient,
+                                borderRadius: BorderRadius.circular(50)),
+                          )),
+                  Positioned.fill(
+                      child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: kDefaultPadding / 2),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                            "${(controller.animation.value * 60).round()} sec"),
+                        SvgPicture.asset('assets/icons/clock.svg')
+                      ],
+                    ),
+                  ))
+                ],
+              );
+            }));
   }
 }
