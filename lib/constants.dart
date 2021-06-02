a4cc75cf7ef70bb14f8abf3d4bf71a22745bd0cd
kreangks <kreangks@bot.or.th>
2021-06-02 10:52:12 +0700

    done welcome_screen.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kSecondaryColor = Color(0xFF8B94BC);
+const kThirdColor = Color(0xFF1C2341);
 const kGreenColor = Color(0xFF6AC259);
 const kRedColor = Color(0xFFE92E30);
 const kGrayColor = Color(0xFFC1C1C1);
--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,12 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_web_02/constants.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Stack(
-        children: [],
-      ),
+      body: Stack(children: <Widget>[
+        SvgPicture.asset(
+          'assets/images/bg.svg',
+          alignment: Alignment.center,
+          width: size.width,
+          height: size.height,
+        ),
+        Container(
+            child: Column(
+          children: <Widget>[
+            Expanded(
+              child: SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Spacer(flex: 2),
+                      Text("Let's Play Quiz,",
+                          style: Theme.of(context).textTheme.headline4),
+                      Text("Enter your information below"),
+                      Spacer(),
+                      TextField(
+                        decoration: InputDecoration(
+                            hintText: "Full Name",
+                            fillColor: kThirdColor,
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(12)))),
+                      ),
+                      Spacer(),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.all(kDefaultPadding * 0.75),
+                          decoration: BoxDecoration(
+                              gradient: kPrimaryGradient,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(12))),
+                          child: Text("Let Start Quiz",
+                              style: TextStyle(color: Colors.black)),
+                        ),
+                      ),
+                      Spacer(
+                        flex: 2,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        )),
+      ]),
     );
   }
 }
