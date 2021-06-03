eded0cc5c5ced0269a14df4e0ef378b59cd92948
kreangks <kreangks@bot.or.th>
2021-06-03 11:25:17 +0700

    add color to ans Icon

--- lib/screens/quiz/components/option.dart
+++ lib/screens/quiz/components/option.dart
@@ -19,7 +19,7 @@ class Option extends StatelessWidget {
     return GetBuilder<QuestionController>(
         init: QuestionController(),
         builder: (qnController) {
-          Color getTheRightColor() {
+          Color getAnsColor() {
             // print('qnController.isAnswered: ${qnController.isAnswered}');
             // print('qnController.correctAns: ${qnController.correctAns}');
             // print('qnController.selectedAns: ${qnController.selectedAns}');
@@ -34,27 +34,38 @@ class Option extends StatelessWidget {
             return kGrayColor;
           }
 
+          IconData getAnsIcon() {
+            print(getAnsColor());
+            return getAnsColor() == kRedColor ? Icons.close : Icons.done;
+          }
+
           return InkWell(
             onTap: press,
             child: Container(
               margin: EdgeInsets.only(top: kDefaultPadding),
               padding: EdgeInsets.all(kDefaultPadding),
               decoration: BoxDecoration(
-                  border: Border.all(color: getTheRightColor()),
+                  border: Border.all(color: getAnsColor()),
                   borderRadius: BorderRadius.circular(15)),
               child: Row(
                 children: [
                   Text(
                     '${index + 1} $text',
-                    style: TextStyle(color: getTheRightColor(), fontSize: 16),
+                    style: TextStyle(color: getAnsColor(), fontSize: 16),
                   ),
                   Spacer(),
                   Container(
                     height: 26,
                     width: 26,
                     decoration: BoxDecoration(
-                        border: Border.all(color: getTheRightColor()),
+                        color: getAnsColor() == kGrayColor
+                            ? Colors.transparent
+                            : getAnsColor(),
+                        border: Border.all(color: getAnsColor()),
                         borderRadius: BorderRadius.circular(50)),
+                    child: getAnsColor() == kGrayColor
+                        ? null
+                        : Icon(getAnsIcon(), size: 16),
                   ),
                 ],
               ),
